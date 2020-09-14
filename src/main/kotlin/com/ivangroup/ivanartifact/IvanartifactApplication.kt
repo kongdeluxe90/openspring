373972b27bfe3c61dc1388ec194810214899644f
Maretic, Ivan <ivan.maretic@lht.dlh.de>
2020-09-14 17:51:41 +0200

    first commit

---
+++ src/main/kotlin/com/ivangroup/ivanartifact/IvanartifactApplication.kt
@@ -0,0 +1,25 @@
+package com.ivangroup.ivanartifact
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+
+@SpringBootApplication
+@RestController
+class IvanartifactApplication {
+	@GetMapping("/hello")
+	fun hello(@RequestParam(value = "name", defaultValue = "World") name: String?): String {
+		return String.format("Hello %s!", name)
+	}
+
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			SpringApplication.run(IvanartifactApplication::class.java, *args)
+		}
+	}
+}
---
+++ src/test/kotlin/com/ivangroup/ivanartifact/IvanartifactApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ivangroup.ivanartifact
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IvanartifactApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
